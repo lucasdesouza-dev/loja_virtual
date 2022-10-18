ddbb639ef2d1d3c71a5de23f3b59283a3721eb44
Lucas Henrique de Souza <luca.s.ouza@hotmail.com>
2022-10-17 23:35:18 -0300

    melhorando tela de login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,19 +2,32 @@ import React from "react";
 
 const Login = () => {
   return (
-    <div className=" bg-gray-700 w-screen h-screen flex justify-center items-center">
-      <div className=" w-2/4 h-80 bg-gray-300">
-        <h1>Faca Login</h1>
-        <div className="w-full h-full flex flex-col  items-center justify-center">
-          <label className=" m-6">
-            <span className="m-6">Email :</span>
-            <input type="email" name="" id="" />
+    <div className=" bg-[#ecf0f1] w-screen h-screen flex justify-center items-center">
+      <div className=" w-2/4 h-2/4 rounded-lg  flex">
+        <div className="w-2/5 h-full bg-[#58af9c] rounded-l-lg"></div>
+        <div className=" rounded-r-lg bg-white w-2/4 h-full flex flex-col  items-center justify-center">
+          <label className=" m-6 w-2/3 h-10">
+            <input
+              type="email"
+              name=""
+              id=""
+              className="bg-gray-300 text-center w-full h-full"
+              placeholder="UserName"
+            />
           </label>
-          <label>
-            <span className="m-6">Senha:</span>
-            <input type="password" name="" id="" />
+          <label className="w-2/3 h-10">
+            <input
+              type="password"
+              name=""
+              id=""
+              className="bg-gray-300 text-center w-full h-full"
+              placeholder="Password"
+            />
           </label>
-          <button type="submit" className=" bg-blue-500 m-6 p-4">
+          <button
+            type="submit"
+            className="w-1/3 bg-blue-500 m-6 p-4 rounded-full"
+          >
             Entrar
           </button>
         </div>
